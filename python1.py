528c482b845e336c6d07ce6f8645dfeae4a56bc2
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-05-04 10:30:39 +0300

    variables learn

---
+++ python1.py
@@ -0,0 +1,12 @@
+
+# arithmetic operators
+    # + Addition
+    # - Subtraction
+    # * Multiplication
+    # / Division
+    # % Mod (the remainder after dividing)
+    # ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
+    # // Divides and rounds down to the nearest integer
+
+# variables
+    
