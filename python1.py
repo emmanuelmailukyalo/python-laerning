9247c10b4ea2caf09048678041a80f741209a9eb
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-05-04 10:32:49 +0300

    variables learn

--- python1.py
+++ python1.py
@@ -9,4 +9,8 @@
     # // Divides and rounds down to the nearest integer
 
 # variables
-    
+    # Only use ordinary letters, numbers and underscores in your variable names. They can’t have spaces, and need to start with a letter or underscore.
+    # You can’t use reserved words or built-in identifiers
+    # use lowercase letters and underscores to separate words 
+
+   
