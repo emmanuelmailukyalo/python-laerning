582801243ddd3c13df181cd6b433cb23ee685894
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-05-10 13:03:27 +0300

    Type and Type Conversion example

--- python1.py
+++ python1.py
@@ -179,5 +179,23 @@ print(tfc)
     print(type(633))  #<class 'int'>
     print(type("633"))  #<class 'str'>
 
+    # Calculate and print the total sales for the week from 
+    # the data provided. Print out a string of the form 
+    # "This week's total sales: xxx", where xxx will be 
+    # the actual total of all the numbers. You’ll need 
+    # to change the type of the input data in order to 
+    # calculate that total.
+
+    mon_sales = "121"
+    tues_sales = "105"
+    wed_sales = "110"
+    thurs_sales = "98"
+    fri_sales = "95"  
+
+    #TODO: Print a string with this format: This week's total sales: xxx
+    # You will probably need to write some lines of code before the print statement.
+    weekly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+    weekly_sales = str(weekly_sales)  #convert the type back!!
+    print("This week's total sales: " + weekly_sales)
     
  
