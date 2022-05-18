d7c54c0d82118c17358bf83fcecb51a1089b4f97
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-05-18 10:31:10 +0300

    slice learn with examples

--- Datastructures.py
+++ Datastructures.py
@@ -13,3 +13,21 @@ print(numbers[2])
 # >>> 3
 print(numbers[-1])  # -1 starts from behind
 # >>> 10
+
+# slice 
+#slice eg
+
+print(numbers[:5])   #outputs
+[1, 2, 3, 4, 5]
+
+print(numbers[5:])  #outputs
+[6, 7, 8, 9, 10]   
+
+print(numbers[0:6])  # u r slicing 0 - 6  but the 6th value is not included it is excluded
+[1, 2, 3, 4, 5, 6]
+
+# here is how 
+print(numbers[0:1])   # we expect the output to be [1, 2] but the upper index is excluded
+[1]
+print(numbers[0:2]) 
+[1, 2]
