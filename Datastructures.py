240806b4c6a7f2e5c39b6c5265ac485bd86bbcf3
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-05-18 09:59:15 +0300

    simple integer list example

--- Datastructures.py
+++ Datastructures.py
@@ -3,3 +3,11 @@
 # lists 
 # lists are is defined using square brackets it has data separated by a comma
 #it can contain all data types
+
+# list example
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers[0])
+# >>> 1
+print(numbers[2])
+# >>> 3
