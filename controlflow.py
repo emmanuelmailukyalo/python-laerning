4c8e5c37ac7a3d3379730c6296698c3c2ac701e1
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-21 10:46:19 +0300

    if statements

---
+++ controlflow.py
@@ -0,0 +1,20 @@
+            # conditional statements 
+        # if statements 
+
+phone_balance = 3
+bank_balance = 100
+
+print(phone_balance, bank_balance)
+
+# write a statement that when the phone balnce is below 5 , 
+# some money (1$) is removed from the bank
+
+if phone_balance < 5:
+    bank_balance -= 10  #use equal to assign the outcome as the new value 
+    phone_balance += 10 
+
+    print(phone_balance, bank_balance)
+    # 13, 90
+
+
+    
