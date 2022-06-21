9eac9fd1fdd58f270679d1898e236fe6fad052b5
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-21 10:51:05 +0300

    elif example

--- controlflow.py
+++ controlflow.py
@@ -17,4 +17,21 @@ if phone_balance < 5:
     # 13, 90
 
 
-    
+
+# Practice: Which Prize
+
+# Write an if statement that lets a competitor know which of these prizes they won based on 
+# the number of points they scored, which is stored in the integer variable points.
+# Points 	Prize
+# 1 - 50 	wooden rabbit
+# 51 - 150 	no prize
+# 151 - 180 	wafer-thin mint
+# 181 - 200 	penguin
+
+# All of the lower and upper bounds here are inclusive, and points can only take on positive
+#  integer values up to 200.
+
+# In your if statement, assign the result variable to a string holding the appropriate message
+# based on the value of points. If they've won a prize, the message should state 
+# "Congratulations! You won a [prize name]!" with the prize name. 
+# If there's no prize, the message should state "Oh dear, no prize this time."
