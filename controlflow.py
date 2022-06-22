6652ee197ff307bc7ec64d424f40e08b9bb35308
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-22 10:40:24 +0300

    Write a program to calculate the electricity bill (accept number of unit from user)

--- controlflow.py
+++ controlflow.py
@@ -140,3 +140,24 @@ if number %7==0:
     print("divisible by seven")
 else:
     print("not divisible by 7")
+
+#  Write a program to calculate the electricity bill (accept number of unit from user)
+#   according to the following criteria :
+
+#      Unit                                                       Price  
+# First 100 units                                               no charge
+# Next 100 units                                              Rs 5 per unit
+# After 200 units                                             Rs 10 per unit
+# (For example if input unit is 350 than total bill amount is Rs2000)
+
+total_bill = 0
+electricity_bill = 150
+
+if electricity_bill >= 100:
+    total_bill= 0
+# elif electricity_bill >= 200:
+#     total_bill=(electricity_bill*5)
+else:
+    total_bill=(electricity_bill*10)
+
+print(total_bill)
