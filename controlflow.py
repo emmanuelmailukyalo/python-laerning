d1f94c9fbc071ab98a056e00a559425453bb5117
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-28 10:55:40 +0300

    leap year question answered

--- controlflow.py
+++ controlflow.py
@@ -244,3 +244,20 @@ print("largest number is:", largest_number)
 # Tip: use the != and % operators.
 
 # Test your code using the data we've provided.
+
+
+year = 2000
+
+	
+if year < 1582:
+    print("Not within the Gregorian calendar period")
+elif year%4!=0:
+    print("common year")
+elif year%100!=0:
+    print("leap year")
+elif year%400!=0:
+    print("common year")
+else:
+    print("leap year")
+
+    
