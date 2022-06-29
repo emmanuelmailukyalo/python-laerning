e8fa2563cb32b2c90b09afaa62e19716f2028cc5
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-29 09:43:58 +0300

    question answered

--- controlflow.py
+++ controlflow.py
@@ -269,3 +269,8 @@ else:
 # Use a for loop to take a list and print each element of the list in its own line. 
 
 sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+# Write a for loop to print out each word in the sentence list, one word per line
+
+for text in sentence:
+    print(text, end="\n")
