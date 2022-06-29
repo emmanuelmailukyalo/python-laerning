987c5ed0f32c932acf5003509e89c84b0a177226
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-29 12:01:59 +0300

    written wrong program expectation. it should read "this is a program that add the digits of a number that has 3 digits"

--- controlflow challenges.py
+++ controlflow challenges.py
@@ -2,7 +2,7 @@
 
 # this is a program that add the digits of any two numbers entered 
 
-num = int(input("enter any number that has two digits"))
+num = int(input("enter any number that has three digits"))
 calc = [int(i) for i in str(num)]
 x,y,z = calc
 print(x+y+z)
