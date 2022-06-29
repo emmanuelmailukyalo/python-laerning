face43d4938987265707298ac2c60af2bde510cf
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-29 09:43:05 +0300

    for loop eaxample question and requrements  stated

--- controlflow.py
+++ controlflow.py
@@ -263,3 +263,9 @@ else:
     # for loop 
 
 # for loop example 
+
+# Practice: Quick Brown Fox
+
+# Use a for loop to take a list and print each element of the list in its own line. 
+
+sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
