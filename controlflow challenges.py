795ca4f85c0960a134f9347776fa15ac1f488c22
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-29 12:00:24 +0300

    his is a program that add the digits of any two numbers entered

--- controlflow challenges.py
+++ controlflow challenges.py
@@ -1,1 +1,8 @@
 # here is where i shall do my python coding practises 
+
+# this is a program that add the digits of any two numbers entered 
+
+num = int(input("enter any number that has two digits"))
+calc = [int(i) for i in str(num)]
+x,y,z = calc
+print(x+y+z)
