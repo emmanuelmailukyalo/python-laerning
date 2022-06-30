04764547dbb9d9cad6cfc5de1f98900fa08e545a
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-06-30 22:35:52 +0300

    create a proram that adds the digits of any number entered

--- controlflow challenges.py
+++ controlflow challenges.py
@@ -6,3 +6,6 @@ num = int(input("enter any number that has three digits"))
 calc = [int(i) for i in str(num)]
 x,y,z = calc
 print(x+y+z)
+
+
+# create a proram that adds the digits of any number entered 
