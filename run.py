0531768752ae6a1076f1d16709a4ee2fe7393aec
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-07-04 10:49:23 +0300

    created the a run file to run the programs externally

---
+++ run.py
@@ -0,0 +1,11 @@
+num = int(input("enter any number that has three digits"))
+calc = [int(i) for i in str(num)]
+x,y,z = calc
+print(x+y+z)
+
+
+# create a proram that adds the digits of any number entered 
+num = int(input("enter any number that has three digits"))
+calc = [int(i) for i in str(num)]
+x,y,z,a,b = calc
+print(x+y+z+a+b)
