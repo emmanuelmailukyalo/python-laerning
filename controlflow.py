08084a1959fdd4818dcc4e5b672bd051e53d4b61
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-07-11 15:45:18 +0300

    spaces

--- controlflow.py
+++ controlflow.py
@@ -528,3 +528,53 @@ print(fruits)
 # use the get function to get the key of sandwiches 
 
 print(fruits.get('sandwiches'))
+
+
+My_lists = [[0,1,2,3] for i in range(2)]
+print(My_lists[2][0])
+
+
+vals = [0,1,2]
+vals.insert(0,1)
+del vals[1]
+
+My_lists = [3,1,-2]
+print(My_lists[My_lists[-1]])
+
+nums = [1,2,3]
+vals = nums 
+del vals[1:2]
+print(nums)
+print(vals)
+
+for i in range (1):
+    print('#')
+else:
+     print('#')
+
+
+My_list = [1,2,3,4]
+print(My_list[-3:-2])
+
+
+z= 10
+y = 10
+x = y<z and z>y or y>z and z<y 
+print(x)
+
+
+x=1
+x=x == x
+
+a =1
+b=0
+c= a & b
+d = a | b
+e = a ^ b
+
+print(c+d+e)
+
+i = 0
+while 1 <= 3:
+    i += 2
+    print("*")
