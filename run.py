01c2d4c49fe8a72dd3d7b1f6e05b3a3faf0090a5
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-07-19 10:16:58 +0300

    mulitiline statement indentation learn

--- run.py
+++ run.py
@@ -9,3 +9,13 @@ num = int(input("enter any number that has three digits"))
 calc = [int(i) for i in str(num)]
 x,y,z,a,b = calc
 print(x+y+z+a+b)
+
+
+
+# python exercises 
+
+string = '''Twinkle, twinkle, little star, How I wonder what you are 
+Up above the world so high, Like a diamond in the sky. Twinkle, 
+twinkle, little star, How I wonder what you are'''
+
+# use the triple quotes to write a multiline sentence 
