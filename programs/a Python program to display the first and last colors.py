a9133c261f4712e5e1266d23ec76fd23eea08197
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-07-19 10:51:27 +0300

    a Python program to display the first and last colors

---
+++ programs/a Python program to display the first and last colors.py
@@ -0,0 +1,5 @@
+# Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+first_color = (color_list[0])
+last_color = (color_list[-1])
+print (first_color,"and", last_color)
