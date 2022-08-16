bc01ad4ac0b507f34a49c29a56f7ce1db240343f
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-16 10:18:21 +0300

    iterations example with use of (split) function

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,6 +1,13 @@
 # warming up 
 
 from netrc import netrc
+from posixpath import split
 # warming up 
 
 # iterations 
+
+verse = "invalid syntax. Perhaps you forgot a comma?"
+verses = verse.split()
+
+for verse in verses:
+    print(verse)
