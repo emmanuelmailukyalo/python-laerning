28b06798a0822d8a4dec69af98949828bd9f98a4
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-16 11:09:12 +0300

    # while loop

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,10 +1,34 @@
 # warming up 
 
-from netrc import netrc
-from posixpath import split
-# warming up 
-
 # iterations 
+# while loop 
 
-for i in range(1,6):
-    print(i)
+# number to find the factorial of
+number = 6
+# start with our product equal to one
+product = 1
+# track the current number being multiplied
+current = 1
+
+while  current <= number:
+    # multiply the product so far by the current number
+    product *= current
+    # increment current with each iteration until it reaches number
+    current += 1
+
+
+# print the factorial of number
+print(product)
+
+
+# number we'll find the factorial of
+number = 6
+# start with our product equal to one
+product = 1
+
+# calculate factorial of number with a for loop
+for num in range(2, number + 1):
+    product *= num
+
+# print the factorial of number
+print(product)
