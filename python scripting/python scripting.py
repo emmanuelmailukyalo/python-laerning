aa585de714b1df810900155d6cf591723ce3131a
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-16 10:08:17 +0300

    iterations

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -3,11 +3,4 @@
 from netrc import netrc
 # warming up 
 
-# selections 
-
-
-# create  a program that tells u the number os servers online
-
-servers = (input("no of servers"))
-
-print( "no of servers", servers)
+# iterations 
