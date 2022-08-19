8661f5579aef1a6d5c4c968899bcdfb146cfb13b
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-19 09:49:41 +0300

    while loop learn

--- controlflow.py
+++ controlflow.py
@@ -528,3 +528,39 @@ print(fruits)
 # use the get function to get the key of sandwiches 
 
 print(fruits.get('sandwiches'))
+
+
+# warming up 
+
+# iterations 
+# while loop 
+
+# number to find the factorial of
+number = 6
+# start with our product equal to one
+product = 1
+# track the current number being multiplied
+current = 1
+
+while  current <= number:
+    # multiply the product so far by the current number
+    product *= current
+    # increment current with each iteration until it reaches number
+    current += 1
+
+
+# print the factorial of number
+print(product)
+
+
+# number we'll find the factorial of
+number = 6
+# start with our product equal to one
+product = 1
+
+# calculate factorial of number with a for loop
+for num in range(2, number + 1):
+    product *= num
+
+# print the factorial of number
+print(product)
