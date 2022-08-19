c7f546eec90e8f0eb716d6987f7da38a4e808abd
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-19 10:10:59 +0300

    created a program that shows the multiplication table for a number

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,7 +1,10 @@
-count = 0
 
-while count < 5:
-    print("this is a loop")
+# create a program that gives the multiplication of the number by 1- 10
+
+number = int(input("enter a number :"))
+
+count = 1 
+while count <= 10:
+    product = number * count 
+    print(number, "x",count, "=", product)
     count += 1
-
-    
