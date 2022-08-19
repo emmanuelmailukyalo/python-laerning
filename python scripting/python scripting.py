d9732bab1f30016679efd281a1239896736100b5
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-19 10:05:17 +0300

    understanding loops

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,34 +1,7 @@
-# warming up 
+count = 0
 
-# iterations 
-# while loop 
+while count < 5:
+    print("this is a loop")
+    count += 1
 
-# number to find the factorial of
-number = 6
-# start with our product equal to one
-product = 1
-# track the current number being multiplied
-current = 1
-
-while  current <= number:
-    # multiply the product so far by the current number
-    product *= current
-    # increment current with each iteration until it reaches number
-    current += 1
-
-
-# print the factorial of number
-print(product)
-
-
-# number we'll find the factorial of
-number = 6
-# start with our product equal to one
-product = 1
-
-# calculate factorial of number with a for loop
-for num in range(2, number + 1):
-    product *= num
-
-# print the factorial of number
-print(product)
+    
