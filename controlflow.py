5bd0778ee731fde59312b0722139b8e2aae9ebd6
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-19 10:11:39 +0300

    create a program that gives the multiplication of the number by 1- 10

--- controlflow.py
+++ controlflow.py
@@ -564,3 +564,14 @@ for num in range(2, number + 1):
 
 # print the factorial of number
 print(product)
+
+
+
+# create a program that gives the multiplication of the number by 1- 10
+
+number = int(input("enter a number :"))
+
+count = 1 
+while count <= 10:
+    print(number, "x",count, "=", product)
+    count += 1
