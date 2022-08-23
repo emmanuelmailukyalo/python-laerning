474066104e8fa006d50eb74d95bfe35602fedffd
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-23 10:44:28 +0300

    Write a loop with a break test completed

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -27,24 +27,3 @@ for headline in headlines :
 
 print(news_ticker)
 print(len(news_ticker))
-
-
-
-
-
-
-# headlines = ["Local Bear Eaten by Man",
-#              "Legislature Announces New Laws",
-#              "Peasant Discovers Violence Inherent in System",
-#              "Cat Rescues Fireman Stuck in Tree",
-#              "Brave Knight Runs Away",
-#              "Papperbok Review: Totally Triffic"]
-
-# news_ticker = ""
-# for headline in headlines:
-#     news_ticker += headline + " "
-#     if len(news_ticker) >= 140:
-#         news_ticker = news_ticker[:140]
-#         break
-
-# print(news_ticker)
