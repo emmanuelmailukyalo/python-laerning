2f766058ccd4d3eed7f2f527bde07bd05deab3bb
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-23 12:15:24 +0300

    program for checcking prime numbers

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,11 +1,17 @@
 
-weathers=["snow", "rain", "sun", "clouds"]
+check_prime = [26, 39, 51, 53, 57, 79, 85]
 
+# iterate through the check_prime list
+for num in check_prime:
 
-#Type your answer here.
-for weather in weathers:
-    if weather == "sun":
-        continue 
-    print(weather)
+# search for factors, iterating through numbers ranging from 2 to the number itself
+    for i in range(2, num):
 
-print(weather)
+# number is not prime if modulo is 0
+        if (num % i) == 0:
+            print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
+            break
+
+# otherwise keep checking until we've searched all possible factors, and then declare it prime
+        if i == num -1:    
+            print("{} IS a prime number".format(num))
