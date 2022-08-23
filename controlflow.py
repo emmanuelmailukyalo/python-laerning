e7039ba7793405812dd7df7b11644c8841315cde
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-23 10:51:25 +0300

    write a program using break that breaks after 5 loops

--- controlflow.py
+++ controlflow.py
@@ -622,3 +622,11 @@ for headline in headlines :
 
 print(news_ticker)
 print(len(news_ticker))
+
+
+# write a program using break that breaks after 5 loops 
+
+for i in range (0,7):
+    if i >= 5 :
+        break
+    print(i)
