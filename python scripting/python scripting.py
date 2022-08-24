388d13b444b020d1e8594f82ec846023e5865d5e
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-24 10:59:22 +0300

    added comments for easy readibilty

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,8 +1,10 @@
 # zip 
 
 shop = list(zip(["mercedes-amg", "jeep-wrangler", "jeep-cherokee", "gmc-yukon",],[2e6,4e6,6e6,20e6]))
-print (shop)
-cars, price = zip(*shop)
-print(cars)  
+print (shop)   #it prints outs the dictionary and the details
+
+cars, price = zip(*shop)  # this splits the dict into cars and price
+print(cars)  # prints out cars only 
+
 for deals,shop in enumerate(shop):
-    print(deals, shop)
+    print(deals, shop)  # prints out the deals in the shop in an ordely format (run to see output)
