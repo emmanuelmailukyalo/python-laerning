236aa5828c0d4cbfcacbd7e0a6b930fa8b529bc1
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-24 10:59:01 +0300

    zip and enumerate

--- controlflow.py
+++ controlflow.py
@@ -666,3 +666,15 @@ print(shop)
 fruit, quantity = zip(*shop)
 print(fruit)
 print(quantity)
+
+
+        # zip and enumerate
+
+shop = list(zip(["mercedes-amg", "jeep-wrangler", "jeep-cherokee", "gmc-yukon",],[2e6,4e6,6e6,20e6]))
+print (shop)   #it prints outs the dictionary and the details
+
+cars, price = zip(*shop)  # this splits the dict into cars and price
+print(cars)  # prints out cars only 
+
+for deals,shop in enumerate(shop):
+    print(deals, shop)  # prints out the deals in the shop in an ordely format (run to see output)
