3f71ebf8e1fb145a42d8a71315332e0e55cf18e8
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-24 10:20:31 +0300

    zip eg

---
+++ python scripting/python scripting.py
@@ -0,0 +1,12 @@
+
+
+fruits = {
+            'pears':1,
+            'peaches':1,
+            'grapes':1,
+            'bananas':1,
+            'apples': 4,
+            'oranges': 19,
+            'kites': 3,
+            'sandwiches': 8
+         }
