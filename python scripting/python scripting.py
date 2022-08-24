4a683c7ce8a3507e216c48a92b81c986e8606e53
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-24 10:55:22 +0300

    learn't zip and enumerate

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,11 +1,8 @@
 # zip 
 
-name = ["pear", "mango"]
-size  = [2,6]
-1
-shop = (list(zip(name, size)))
-print(shop)
-
-fruit, quantity = zip(*shop)
-print(fruit)
-print(quantity)
+shop = list(zip(["mercedes-amg", "jeep-wrangler", "jeep-cherokee", "gmc-yukon",],[2e6,4e6,6e6,20e6]))
+print (shop)
+cars, price = zip(*shop)
+print(cars)  
+for deals,shop in enumerate(shop):
+    print(deals, shop)
