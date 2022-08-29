8708212ca513ec1e70f53b36a878946819f1f898
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-29 10:41:04 +0300

    zip and dict functions create a dictionary which has its key-value pairs coming from lst1 and lst2.

--- controlflow.py
+++ controlflow.py
@@ -678,3 +678,17 @@ print(cars)  # prints out cars only
 
 for deals,shop in enumerate(shop):
     print(deals, shop)  # prints out the deals in the shop in an ordely format (run to see output)
+
+
+# Using zip and dict functions create a dictionary which has its key-value 
+# pairs coming from lst1 and lst2.
+
+lst1=["Netflix", "Hulu", "Sling", "Hbo"]
+lst2=[198, 166, 237, 125]
+
+#Type your answer here.
+
+
+points= dict(zip(lst1,lst2))
+
+print(points)
