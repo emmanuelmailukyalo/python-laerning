9831a89e6bbd40f6816b50eec506f11360623e68
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-29 10:40:19 +0300

    list comprehensions

--- python scripting/python scripting.py
+++ python scripting/python scripting.py
@@ -1,12 +1,16 @@
-# Using zip and dict functions create a dictionary which has its key-value 
-# pairs coming from lst1 and lst2.
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
 
-lst1=["Netflix", "Hulu", "Sling", "Hbo"]
-lst2=[198, 166, 237, 125]
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
 
-#Type your answer here.
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
 
-
-points= dict(zip(lst1,lst2))
-
-print(points)
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
