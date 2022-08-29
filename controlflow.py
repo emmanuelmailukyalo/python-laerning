fc63d9aa3addcd9b1b22b0c654f960c674da8474
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-08-29 10:41:30 +0300

    indentation and spacing correct

--- controlflow.py
+++ controlflow.py
@@ -686,9 +686,6 @@ for deals,shop in enumerate(shop):
 lst1=["Netflix", "Hulu", "Sling", "Hbo"]
 lst2=[198, 166, 237, 125]
 
-#Type your answer here.
-
-
 points= dict(zip(lst1,lst2))
 
 print(points)
