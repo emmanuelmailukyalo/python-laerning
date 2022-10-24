d2a28fa6139cac3f86a1691d45532034a9322b26
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-10-24 09:00:22 +0300

    palindrome

--- arrow challenge.py
+++ arrow challenge.py
@@ -8,3 +8,10 @@ print("  *   *")
 print("  *****")
 
 #this prints out an arrow
+
+
+word = str(input("enter a word"))
+
+if word == "palpaindrome":
+    print("is palindrome")
+else: print("write palindrome")
