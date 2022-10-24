496f3b166bcd28059d3078664109390322ade019
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-10-24 09:05:21 +0300

    palindrome challenge ousted

--- arrow challenge.py
+++ arrow challenge.py
@@ -1,14 +1,4 @@
 
-#this prints out an arrow
-
-
-word = str(input("enter a word"))
-
-if word == "palindrome":
-    print("is palindrome")
-else: print("write palindrome")
-
-
 # print arrow 
 
 print("    *")
