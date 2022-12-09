db9f172c6475f6085130e556c383e57aa744be07
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-12-09 10:54:55 +0300

    party planner horiblre fail

--- scripting.py
+++ scripting.py
@@ -1,8 +1,13 @@
 
-people = int(input("enternoof pple "))
-cookies = int(input("enternoof cookies"))
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
 
+    try:
+        num_each = cookies // people
+        leftovers = cookies % people
+    except ZeroDivisionError:
+        print("Oops, you entered 0 people will be attending.")
+        print("Please enter a good number of people for a party.")
 
-party_planner = people, cookies
-cookieperperson =  people/cookies
-print(cookieperperson)
+    return(num_each, leftovers)
