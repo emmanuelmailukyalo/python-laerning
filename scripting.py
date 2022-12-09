186fedf5b0755e60733ab0673c888a070ad2d04f
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2022-12-09 09:26:22 +0300

    raw input learn

--- scripting.py
+++ scripting.py
@@ -1,16 +1,2 @@
-how_many_snakes = 1
-snake_string = """
-Welcome to Python3!
-
-             ____
-            / . .\\
-            \  ---<
-             \  /
-   __________/ /
--=:___________/
-
-<3, Juno
-"""
-
-
-print(snake_string * how_many_snakes)
+name = input("enter your name")
+print ("your name is" +  name)
